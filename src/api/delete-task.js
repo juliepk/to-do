6adc4dabb35a4984a89b067bd5cd4ac533db2c64
juliepk <juliepiroskakerek@gmail.com>
2022-10-26 20:32:45 +0200

    Update delete-task.js

--- src/api/delete-task.js
+++ src/api/delete-task.js
@@ -1,5 +1,5 @@
 export default function deleteTask(task) {
-	return fetch(`http://localhost:1337/tasks/${task.id}`, {
+	return fetch("http://localhost:1337/tasks", {
 		method: "DELETE",
 		headers: { "Content-Type": "application/json" }
 	})
