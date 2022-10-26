dbf5bb2275c9b45452abd37712d465412a173e50
juliepk <juliepiroskakerek@gmail.com>
2022-10-26 20:32:18 +0200

    Update update-task.js

--- src/api/update-task.js
+++ src/api/update-task.js
@@ -1,5 +1,5 @@
 export default function updateTask(task) {
-	return fetch(`http://localhost:1337/tasks/${task.id}`, {
+	return fetch("http://localhost:1337/tasks", {
 		method: "PUT",
 		headers: { "Content-Type": "application/json" },
 		body: JSON.stringify(task)
