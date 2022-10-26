569c257c8a38eabbc08a0af68b11ff49544d872f
juliepk <juliepiroskakerek@gmail.com>
2022-10-26 20:16:40 +0200

    Update add-task.js

--- src/api/add-task.js
+++ src/api/add-task.js
@@ -1,5 +1,5 @@
 export default function addTask(task) {
-	return fetch(`localhost:1337/tasks`, {
+	return fetch(`http://localhost:1337/tasks`, {
 		method: "POST",
 		headers: { "Content-Type": "application/json" },
 		body: JSON.stringify(task)
